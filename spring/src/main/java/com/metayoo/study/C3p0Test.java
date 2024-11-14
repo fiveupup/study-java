08bfacf6347634706f49e1b0c37026baee4ca3d2
fiveupup <metayoo@outlook.com>
2024-11-14 21:47:28 +0800

    test

---
+++ spring/src/main/java/com/metayoo/study/C3p0Test.java
@@ -0,0 +1,15 @@
+package com.metayoo.study;
+
+import javax.sql.DataSource;
+
+public class C3p0Test {
+    public static void main(String[] args) {
+
+    }
+
+    private DataSource dataSource() {
+//        DataSource dataSource = new ComboPooledDataSource();
+        return null;
+    }
+
+}
